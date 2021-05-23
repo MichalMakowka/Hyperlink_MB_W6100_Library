8f07bf38fdc4ef89bd6c28ff42f17c54799d1974
Michal Makowka <76627690+MichalMakowka@users.noreply.github.com>
2021-05-23 20:45:07 +0100

    Initial commit

---
+++ Core/Src/main.c
@@ -0,0 +1,21 @@
+/*
+ * This is W6100 Hyperlink Motherboard Ethernet library.
+ * Designed by Michal Makowka
+ *
+ * (C) All rights reserved.
+ *
+ */
+
+#include "stm32f405xx.h"
+
+int main(void)
+{
+
+  while (1)
+  {
+
+
+  }
+
+}
+
